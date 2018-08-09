3f7f658ba7fa88bc6b2bc0efbdaff3f5ce63e449
RossPin <dorpetz@hotmail.com>
2018-08-09 17:35:28 +1200

    make project img link

--- index.js
+++ index.js
@@ -1,47 +1,5 @@
 document.addEventListener('DOMContentLoaded', start)
 
-const reactProjects = [
-  {
-    name: 'The Persistence',
-    img: 'placeholder.png'
-  },
-  {
-    name: 'Battle Ships',
-    img: 'placeholder.png'
-  }
-]
-
-const preBootProjects = [
-  {
-    name: 'Mine Sweeper',
-    img: 'placeholder.png'
-  },
-  {
-    name: 'Calculator',
-    img: 'placeholder.png'
-  },
-  {
-    name: 'Piano',
-    img: 'placeholder.png'
-  },
-  {
-    name: 'Local Weather',
-    img: 'placeholder.png'
-  },
-  {
-    name: 'Tic Tac Toe',
-    img: 'placeholder.png'
-  },
-  {
-    name: 'Pomodoro Clock',
-    img: 'placeholder.png'
-  },
-  {
-    name: 'Simon Game',
-    img: 'placeholder.png'
-  }
-]
-
 function start(){
   fillReact()
   fillPreBoot() 
@@ -64,7 +22,8 @@ function fillPreBoot() {
 function addProject(section, element){
   section.innerHTML += `
     <div class="project">
-      <img class="projectImg" src="images/${element.img}" alt="">
+      <a href="${element.url}" target="_blank">
+      <img class="projectImg" src="images/${element.img}" alt=""></a>
       <h4 class="noMargin">${element.name}</h4>
     </div>`
 }
---
+++ projectData.js
@@ -0,0 +1,55 @@
+const reactProjects = [
+  {
+    name: 'The Persistence',
+    img: 'placeholder.png',
+    url: 'https://rosspin.github.io/'
+  },
+  {
+    name: 'Battle Ships',
+    img: 'placeholder.png',
+    url: 'https://rosspin.github.io/'
+  }
+]
+
+const preBootProjects = [
+  {
+    name: 'EDA Blog',
+    img: 'placeholder.png',
+    url: 'https://rosspin.github.io/'
+  },
+  {
+    name: 'Mine Sweeper',
+    img: 'placeholder.png',
+    url: 'https://rosspin.github.io/'
+  },
+  {
+    name: 'Calculator',
+    img: 'placeholder.png',
+    url: 'https://rosspin.github.io/'
+  },
+  {
+    name: 'Piano',
+    img: 'placeholder.png',
+    url: 'https://rosspin.github.io/'
+  },
+  {
+    name: 'Local Weather',
+    img: 'placeholder.png',
+    url: 'https://rosspin.github.io/'
+  },
+  {
+    name: 'Tic Tac Toe',
+    img: 'placeholder.png',
+    url: 'https://rosspin.github.io/'
+  },
+  {
+    name: 'Pomodoro Clock',
+    img: 'placeholder.png',
+    url: 'https://rosspin.github.io/'
+  },
+  {
+    name: 'Simon Game',
+    img: 'placeholder.png',
+    url: 'https://rosspin.github.io/'
+  }
+]
