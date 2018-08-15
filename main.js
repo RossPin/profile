ac778c3cfb39a708969ebcdbbf40146e676f52af
RossPin <dorpetz@hotmail.com>
2018-08-15 12:11:12 +1200

    add tooltip to projects for descriptions

--- main.js
+++ main.js
@@ -23,7 +23,10 @@ function addProject(section, element){
   section.innerHTML += `
     <div class="project">
       <a href="${element.url}" target="_blank">
+      <div class="tooltip">
       <img class="projectImg" src="images/${element.img}" alt=""></a>
+      <p class="tooltiptext">${element.description}</p>
+      </div>
       <h4 class="noMargin">${element.name}</h4>
     </div>`
 }
--- projectData.js
+++ projectData.js
@@ -2,12 +2,14 @@ const reactProjects = [
   {
     name: 'The Persistence',
     img: 'persistence.png',
-    url: 'https://the-persistence.herokuapp.com/'
+    url: 'https://the-persistence.herokuapp.com/',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Sed ea mala virtuti magnitudine obruebantur. Poterat autem inpune; Duo Reges: constructio interrete.'
   },
   {
     name: 'Battle Ships',
     img: 'battleships.png',
-    url: 'https://battleships-eda.herokuapp.com/'
+    url: 'https://battleships-eda.herokuapp.com/',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Sed ea mala virtuti magnitudine obruebantur. Poterat autem inpune; Duo Reges: constructio interrete.'
   }
 ]
 
@@ -15,46 +17,55 @@ const preBootProjects = [
   {
     name: 'EDA Blog',
     img: 'blog.png',
-    url: 'https://rosspin.github.io/'
+    url: 'https://rosspin.github.io/',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Sed ea mala virtuti magnitudine obruebantur. Poterat autem inpune; Duo Reges: constructio interrete.'
   },
   {
     name: 'Mine Sweeper',
     img: 'minesweeper.png',
-    url: 'https://rosspin.github.io/minesweeper/'
+    url: 'https://rosspin.github.io/minesweeper/',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Sed ea mala virtuti magnitudine obruebantur. Poterat autem inpune; Duo Reges: constructio interrete.'
   },
   {
     name: 'Calculator',
     img: 'calculator.png',
-    url: 'https://rosspin.github.io/js-calculator'
+    url: 'https://rosspin.github.io/js-calculator',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Sed ea mala virtuti magnitudine obruebantur. Poterat autem inpune; Duo Reges: constructio interrete.'
   },
   {
     name: 'Piano',
     img: 'piano.png',
-    url: 'https://rosspin.github.io/piano'
+    url: 'https://rosspin.github.io/piano',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Sed ea mala virtuti magnitudine obruebantur. Poterat autem inpune; Duo Reges: constructio interrete.'
   },
   {
     name: 'Local Weather',
     img: 'weather.png',
-    url: 'https://rosspin.github.io/weather'
+    url: 'https://rosspin.github.io/weather',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Sed ea mala virtuti magnitudine obruebantur. Poterat autem inpune; Duo Reges: constructio interrete.'
   },
   {
     name: 'Tic Tac Toe',
     img: 'tictactoe.png',
-    url: 'https://rosspin.github.io/tic-tac-toe'
+    url: 'https://rosspin.github.io/tic-tac-toe',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Sed ea mala virtuti magnitudine obruebantur. Poterat autem inpune; Duo Reges: constructio interrete.'
   },
   {
     name: 'Pomodoro Clock',
     img: 'pomodoro.png',
-    url: 'https://rosspin.github.io/pomodoro'
+    url: 'https://rosspin.github.io/pomodoro',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Sed ea mala virtuti magnitudine obruebantur. Poterat autem inpune; Duo Reges: constructio interrete.'
   },
   {
     name: 'Simon Game',
     img: 'simon.png',
-    url: 'https://rosspin.github.io/Simon'
+    url: 'https://rosspin.github.io/Simon',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Sed ea mala virtuti magnitudine obruebantur. Poterat autem inpune; Duo Reges: constructio interrete.'
   },
   {
     name: 'Conways Game of Life',
     img: 'conways.png',
-    url: 'https://rosspin.github.io/Conways/'
+    url: 'https://rosspin.github.io/Conways/',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Sed ea mala virtuti magnitudine obruebantur. Poterat autem inpune; Duo Reges: constructio interrete.'
   }
 ]
