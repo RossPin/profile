de8231e372f01a638a7f0b5095b18d3d60a1cb6e
RossPin <dorpetz@hotmail.com>
2018-08-17 12:55:40 +1200

    include project description tooltip as part of link

--- main.js
+++ main.js
@@ -23,10 +23,11 @@ function addProject(section, element){
   section.innerHTML += `
     <div class="project">
       <a href="${element.url}" target="_blank">
-      <div class="tooltip">
-      <img class="projectImg" src="images/${element.img}" alt=""></a>
-      <p class="tooltiptext">${element.description}</p>
-      </div>
+        <div class="tooltip">
+          <img class="projectImg" src="images/${element.img}" alt="">
+          <p class="tooltiptext unselectable">${element.description}</p>
+        </div>
+      </a>
       <h4 class="noMargin">${element.name}</h4>
     </div>`
 }
